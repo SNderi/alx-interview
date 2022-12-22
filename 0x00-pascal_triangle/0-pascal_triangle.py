fc37a7cffc007b9bb044ea9b5912b2e4c0e6faa8
SNderi <nderisharonw@gmail.com>
2022-12-22 17:06:49 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,13 +11,13 @@ def pascal_triangle(n):
     elif n == 1:
         pascal = [1]
     else:
-        pascal = [ [1], [1, 1]]
+        pascal = [[1], [1, 1]]
         row = [1, 1]
         i = 1
         while i < n - 1:
             j = -1
             approw = []
-            while j < len(row):
+            while j < (len(row) + 1) // 2:
                 if j < 0:
                     approw.append(row[j + 1])
                 elif j + 1 >= len(row):
@@ -25,8 +25,15 @@ def pascal_triangle(n):
                 else:
                     approw.append(row[j] + row[j + 1])
                 j += 1
+            ch = (len(row)+1) // 2
+            if (len(row) + 1) % 2 == 0:
+                app = approw[:ch - 1]
+                approw = approw + app[::-1]
+            else:
+                app = approw[:ch]
+                approw = approw + app[::-1]
             row = approw
             pascal.append(approw)
             i += 1
 
-    return (pascal)
+    return pascal
