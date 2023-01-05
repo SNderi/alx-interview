5bf42f7e15ffac03a9393c67d08806cf4fcf18bf
Sharon Nderi <nderisharonw@gmail.com>
2023-01-05 15:47:52 +0300

    Handle empty box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,6 +6,8 @@ all boxes can be opened.
 
 def canUnlockAll(boxes):
     """ A method that determines if all the boxes can be opened. """
+    if len(boxes) == 0:
+        return False
     for n in range(1, len(boxes)):
         subset = set(sum((boxes[:n] + boxes[n + 1:]), []))
         if n not in subset:
