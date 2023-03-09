ae7927deb7db4591b6cf0b156db12c7db21d479a
Sharon Nderi <nderisharonw@gmail.com>
2023-03-09 16:51:43 +0300

    Alx interview tasks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -24,7 +24,7 @@ def validUTF8(data):
     """
     n_bytes = 0
     for num in data:
-        bin_string = format(num, '#010b')[-8:]
+        bin_string = _(num)
         if n_bytes == 0:
             for bit in bin_string:
                 if bit == '0':
